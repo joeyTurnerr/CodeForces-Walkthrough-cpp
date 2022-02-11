51ded1c61f617eae7610acd53565cbe22319c993
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-11 02:16:40 +0000

    Create D. Difference.cpp

---
+++ D. Difference.cpp
@@ -0,0 +1,12 @@
+//D. Difference
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long a, b, c, d;
+
+    cin >>a>>b>>c>>d;
+    cout<< "Difference = " <<(a*b)-(c*d);
+    return 0;
+}
