4323aa24aaef37ae4a6d66cc12994af111d6a5f7
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-15 00:55:44 +0000

    Create V. Comparison.cpp

---
+++ V. Comparison.cpp
@@ -0,0 +1,22 @@
+//V. Comparison
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int a, b;
+    char s;
+
+    cin >> a >> s >> b;
+    if (s == '<' && a < b)
+        cout << "Right";
+    else if (s == '>' && a > b)
+        cout << "Right";
+    else if (s == '=' && a == b)
+        cout << "Right";
+    else
+        cout << "Wrong";
+
+
+    return 0;
+}
