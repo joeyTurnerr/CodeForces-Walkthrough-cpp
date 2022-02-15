a6cadec991ec2a8a792ff2e29d31b383421c7a1a
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-15 00:52:54 +0000

    Create Q. Coordinates of a Point.cpp

---
+++ Q. Coordinates of a Point.cpp
@@ -0,0 +1,26 @@
+//Q. Coordinates of a Point
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    double x, y;
+
+    cin >> x >> y;
+
+    if (x == 0 && y ==0)
+        cout << "Origem";
+    else if (y == 0)
+        cout << "Eixo X";
+    else if (x == 0)
+        cout << "Eixo Y";
+    else if (x > 0 && y > 0)
+        cout << "Q1";
+    else if (x < 0 && y > 0)
+        cout << "Q2";
+    else if (x < 0 && y < 0)
+        cout << "Q3";
+    else
+        cout << "Q4";
+    return 0;
+}
