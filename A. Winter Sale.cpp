b5f88996fd4fa33ee987fdd4bd05cdaf2976586f
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-16 08:03:19 +0000

    Update A. Winter Sale.cpp

--- A. Winter Sale.cpp
+++ A. Winter Sale.cpp
@@ -3,6 +3,13 @@
 #include <iomanip>
 using namespace std;
 
+double round (double x)
+{
+    double value = (int) (x*100 +.5);
+
+    return (double) value/100;
+}
+
 int main ()
 {
     double x, p;
@@ -10,4 +17,8 @@ int main ()
     cin >> x >> p;
     cout <<fixed<<setprecision(2)<< p/(1-x/100);
     return 0;
+    
+    //another solution
+    double result = y/ (1-x/100);
+    cout << round(result);
 }
