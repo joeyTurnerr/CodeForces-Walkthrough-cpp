74af24f08c9414bc185d86b58f3f7d6b2ee7eaf7
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-02-23 05:40:52 +0000

    Create 231A-Team.cpp

---
+++ 231A-Team.cpp
@@ -0,0 +1,23 @@
+//231A-Team
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int n, a, b, c, problem{0};
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a >> b >> c;
+        bool twice = (a == b && b == 1)  || (c == b && b == 1) || (a == c && c == 1) ;
+        bool full =  (a == b && b == c && a == 1);
+
+        if (twice || full)
+            problem++;
+    }
+    cout << problem;
+
+    return 0;
+}
