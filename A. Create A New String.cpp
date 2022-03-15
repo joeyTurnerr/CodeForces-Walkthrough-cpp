ce9d67919e767424aeea9b8ce93880fefc6e31e7
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-03-15 20:26:17 +0000

    Create A. Create A New String.cpp

---
+++ A. Create A New String.cpp
@@ -0,0 +1,12 @@
+//A. Create A New String
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+    string s, t;
+    cin >> s >> t;
+    cout << s.length() << " " << t.length() <<'\n';
+    cout << s << " " << t;
+    return 0;
+}
