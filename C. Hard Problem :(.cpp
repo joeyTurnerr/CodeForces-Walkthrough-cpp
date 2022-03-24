ad78e12f5c562d1147fcc5eefdf45ba2edd52b33
joeyTurnerr <99365103+joeyTurnerr@users.noreply.github.com>
2022-03-24 12:08:15 +0000

    Create C. Hard Problem :(.cpp

---
+++ C. Hard Problem :(.cpp
@@ -0,0 +1,17 @@
+//C. Hard Problem :(
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int x; cin >>x;
+    if (x == 25) cout << "Christmas";
+    if (x == 24) cout << "Christmas Eve";
+    if (x == 23) cout << "Christmas Eve Eve";
+    if (x == 22) cout << "Christmas Eve Eve Eve";
+
+    return 0;
+}
+
+
+
